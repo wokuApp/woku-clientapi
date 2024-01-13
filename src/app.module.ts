5895207f00ab73b46d61284b140388fd868e2b48
diorrego <diorrego1@gmail.com>
2024-01-13 20:31:01 -0300

    :art: config swagger an cors

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MulterModule } from '@nestjs/platform-express';
+import { memoryStorage } from 'multer';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { WokusModule } from './wokus/wokus.module';
 
 @Module({
-  imports: [WokusModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    WokusModule,
+    MulterModule.register({
+      storage: memoryStorage(),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
